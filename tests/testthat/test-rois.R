e981e09910bbe3d14cb00bf5b2d9f420d8410587
Romain Franconville <romain.franconville@gmail.com>
2020-08-06 15:01:43 -0700

    Add test for getTypesInRoiTable and co.

--- tests/testthat/test-rois.R
+++ tests/testthat/test-rois.R
@@ -3,3 +3,10 @@ test_that("ROI outline methods match", {
   noOutline <- roiOutline(no_mesh,roiName="NO(R)")
   expect_equal(noOutline,roiOutline("NO(R)"))
 })
+
+test_that("ROI innervation functions work",{
+  expect_is(BULTable <- getTypesInRoiTable("BU(L)",lateralize=TRUE),"neuronBag")
+  expect_true(nrow(BULTable$outputs)>50)
+  expect_is(BULTypes <- typesInROI(BULTable,"BU(L)"),"data.frame")
+  expect_true("ExR1_L" %in% BULTypes$type)
+})
